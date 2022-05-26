fb0c291dbba2b32afd54777ebe289e56c04f76ca
Sharon Ibejih <sharonkcee@gmail.com>
2022-05-26 09:59:12 +0000

    Update word_occurances.py

--- unique-words-counter/word_occurances.py
+++ unique-words-counter/word_occurances.py
@@ -1,5 +1,4 @@
-# Read text from a file, and count the occurence of words 
-# in that text
+# Read text from a file, and count the occurence of words in that text
 
 import string
 
@@ -9,9 +8,9 @@ def read_file_content(filename):
     return texts
 
 
-def count_words():
+def count_words(filename):
     dict_ = {}
-    text = str(read_file_content("./story.txt"))
+    text = str(read_file_content(filename))
     # remove punctuations in text
     text = text.translate(str.maketrans('', '', string.punctuation)).replace("\n","")
     # split texts into list of words
@@ -22,7 +21,8 @@ def count_words():
         count_ = t_list.count(word)
         count_dict = {word:count_}
         dict_.update(count_dict)
-    # sort dict by values and return the output
+    # sort dict by values and returns the occurence of unique words in the text
     return {k: v for k, v in sorted(dict_.items(), key=lambda item: item[1])}
 
-print(count_words())
+# Implementation
+print(count_words("./story.txt"))
